8f263724a4562a9d3d9fc483c8d6ddaf51b81df1
Vexzure <vexzure@proton.me>
2025-01-26 09:53:27 +0100

    [ + ] Push

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://idkaccount3737.github.io/MMBB",
+  base: "https://idkaccount3737.github.io/MMBB/",
 
   optimizeDeps: {
         exclude: ['lucide-react'],
